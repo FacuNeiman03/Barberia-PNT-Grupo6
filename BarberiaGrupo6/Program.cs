6ca48958c500a013c7336092e3dc725f0c580d56
FennnPachu <mirilianomijaslive@gmail.com>
2022-10-20 01:47:33 -0300

    remove comments

--- BarberiaGrupo6/Program.cs
+++ BarberiaGrupo6/Program.cs
@@ -6,14 +6,15 @@ namespace BarberiaGrupo6
     {
         static void Main(string[] args)
         {
-            Barberia barberia = new Barberia("LaBarbería");
-            Cliente cliente1 = new Cliente(1, "Facundo", "Neiman", "1547854852", "123456");
-            Empleado empleado1 = new Empleado(1, "Santiago", "Varela");
-            Persona cliente2 = new Cliente(1, "Sacha", "Piszyck", "54564654654", "321456");
-            Sucursal sucursal1 = new Sucursal(1, "Angel Gallardo", 1006);
-            DateTime fecha = new DateTime(2022, 10, 22, 15, 30, 0);
-            Turno turno = new Turno(fecha, MetodoPago.EFECTIVO, sucursal1, cliente1);
-            turno.mostrar();
+            /*      Barberia barberia = new Barberia("LaBarbería");
+                  Cliente cliente1 = new Cliente(1, "Facundo", "Neiman", "1547854852", "123456");
+                  Empleado empleado1 = new Empleado(1, "Santiago", "Varela");
+                  Persona cliente2 = new Cliente(1, "Sacha", "Piszyck", "54564654654", "321456");
+                  Sucursal sucursal1 = new Sucursal(1, "Angel Gallardo", 1006);
+                  DateTime fecha = new DateTime(2022, 10, 22, 15, 30, 0);
+                  Turno turno = new Turno(fecha, MetodoPago.EFECTIVO, sucursal1, cliente1);
+                  turno.mostrar();*/
+            Console.WriteLine("Hola");
         }
     }
 }
